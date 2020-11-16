b501267910eb95173dab31bfb41abdfd24d18275
LuGew <lugew@outlook.com>
2020-11-16 17:58:22 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import VueI18n from "vue-i18n";
+import 'ant-design-vue/dist/antd.css';
 
 Vue.config.productionTip = false
 
+Vue.use(Antd);
+Vue.use(VueI18n)
+
+const messages = {
+    zh: {
+        input: "输入",
+        username: "用户名",
+        password: "密码",
+        login: "登陆",
+        please_input_username: "请输入用户名",
+        please_input_password: "请输入密码"
+    },
+    en: {}
+}
+
+const i18n = new VueI18n({
+    locale: "zh",
+    messages
+});
+
 new Vue({
-  render: h => h(App),
+    i18n,
+    render: h => h(App),
 }).$mount('#app')
+
+
---
+++ src/views/login/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <a-form layout="horizontal">
+    <a-form-item
+        :label="$t('username')"
+        :label-col="formItemLayout.labelCol"
+        :wrapper-col="formItemLayout.wrapperCol"
+    >
+      <a-input :placeholder="$t('please_input_username')"/>
+    </a-form-item>
+    <a-form-item
+        :label="$t('password')"
+        :label-col="formItemLayout.labelCol"
+        :wrapper-col="formItemLayout.wrapperCol"
+    >
+      <a-input :placeholder="$t('please_input_password')"/>
+    </a-form-item>
+    <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
+      <a-button type="primary">
+        {{ $t('login') }}
+      </a-button>
+    </a-form-item>
+  </a-form>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      formLayout: 'horizontal',
+    };
+  },
+  computed: {
+    formItemLayout() {
+      const {formLayout} = this;
+      return formLayout === 'horizontal'
+          ? {
+            labelCol: {span: 4},
+            wrapperCol: {span: 14},
+          }
+          : {};
+    },
+    buttonItemLayout() {
+      const {formLayout} = this;
+      return formLayout === 'horizontal'
+          ? {
+            wrapperCol: {span: 14, offset: 4},
+          }
+          : {};
+    },
+  },
+  methods: {
+    handleFormLayoutChange(e) {
+      this.formLayout = e.target.value;
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
